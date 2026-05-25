18774748818f059730ae3f4031db44fe84ba702d
santhosh-vavilala <santhosh-vavilala@users.noreply.github.com>
2026-05-25 18:05:36 +0000

    deploy: 558ef99cb7071f05071c95905268a2fde7bdd79b

---
+++ server/assets/_tanstack-start-manifest_v-Bob_oDGX.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/home/runner/work/school-connect-hub/school-connect-hub/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-tifzf8Ux.js"] }, "/": { filePath: "/home/runner/work/school-connect-hub/school-connect-hub/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-B0KoRLDO.js"] } }, clientEntry: "/assets/index-tifzf8Ux.js" });
+export {
+  tsrStartManifest
+};
---
+++ server/index.js
@@ -0,0 +1,5 @@
+import { w } from "./assets/worker-entry-D313STsS.js";
+import "node:events";
+export {
+  w as default
+};
